e1e2444ae30d952bc3e5721fe909486de882a662
Aidan Cunniffe <acunniffe@gmail.com>
2026-02-17 19:06:56 -0500

    squashed

--- src/commands/blame.rs
+++ src/commands/blame.rs
@@ -316,8 +316,7 @@ impl Repository {
                 )));
             }
 
-            let raw_bytes = fs::read(&abs_file_path)?;
-            let content = String::from_utf8_lossy(&raw_bytes).into_owned();
+            let content = fs::read_to_string(&abs_file_path)?;
             let lines_count = content.lines().count() as u32;
             (content, lines_count)
         };
@@ -471,7 +470,7 @@ impl Repository {
         } else {
             exec_git(&args)?
         };
-        let stdout = String::from_utf8_lossy(&output.stdout).into_owned();
+        let stdout = String::from_utf8(output.stdout)?;
 
         // Parser state for current hunk
         #[derive(Default)]
